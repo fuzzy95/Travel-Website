60775f21f8ec572103b14d2146128c31fcb6686d
fuzzy95 <asif.eshaq@northsouth.edu>
2020-09-09 15:38:47 +0600

    completed header section

---
+++ travel/src/components/MainSection.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import '../App.css';
+import { Button } from './Button';
+import './MainSection.css';
+
+function MainSection() {
+    return (
+        <div className="main_container">
+            <video src="/videos/video-2.mp4" autoPlay loop muted />
+
+            <h1>DIVE IN TO EXPLORE.</h1>
+            <p>What are you waiting for?</p>
+
+            <div className="main_btns">
+                <Button className="btns" buttonStyle="btn__outline" buttonSize="btn__large">
+                    GET STARTED
+                </Button>
+
+                <Button className="btns" buttonStyle="btn__primary" buttonSize="btn__large">
+                    WATCH TRAILER <i className="far fa-play-circle" />
+                </Button>
+            </div>
+        </div>
+    );
+}
+
+export default MainSection;
